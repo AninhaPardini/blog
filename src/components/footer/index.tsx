2e3fc9f25ac6d034822890b214d5413705df4c96
AninhaPardini <aninhapardini.work@gmail.com>
2024-07-29 03:26:49 -0300

    feat: adicionado cor e hover aos links

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -20,10 +20,13 @@ const socials = [
 export default function Footer() {
   return (
     <footer className="bg-pink-500 py-4">
+      <hr
+        className='text-white-200 w-full mb-16' 
+      />
       <div className="flex justify-center space-x-4">
         {socials.map((social) => (
           <Link key={social.name
-          } className='capitalize' href={social.link} target="_blank" rel="noopener noreferrer">
+          } className='capitalize text-mandy-500 hover:text-mandy-800' href={social.link} target="_blank" rel="noopener noreferrer">
             {/* <FontAwesomeIcon icon={social.name} className="h-6 w-6 text-white" /> */}
             {social.name}
           </Link>
