3a00a8b25c7c5342506e13ed6b27707b4f0a0090
AninhaPardini <anapardinisimoni@gmail.com>
2024-12-26 18:32:34 -0300

    fix: ajustado posts no modo responsivo

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -8,10 +8,10 @@ export default function PostPreview() {
   return (
     <div className="flex flex-col justify-center items-start py-16 w-full gap-8">
       <div>
-        <h1 
-          className="flex justify-center items-center gap-2 text-4xl font-bold text-white-950"
+        <h1
+          className="flex justify-center items-center gap-2 text-2xl md:text-4xl font-bold text-white-950"
         >
-          <NewspaperIcon className="w-12 mr-2" />
+          <NewspaperIcon className=" w-8 md:w-12 md:mr-2" />
           Últimos posts
         </h1>
       </div>
@@ -19,35 +19,35 @@ export default function PostPreview() {
         <section className="bg-white">
           <div className="lg:py-16">
             <div className="grid gap-8 lg:grid-cols-3">
-                <ArticlePreview 
-                  banner="https://images.unsplash.com/photo-1648854006531-361649aa182c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-                  icon={Article} date="Hoje" 
-                  link="posts/figma-fundamentos" 
-                  title="Figma para começar bem" 
-                  description="Esse post é para você que já se aventurou no figma mas não sabe aplicar as melhores práticas para otimizar sua vida ou de sua equipe."
-                  colorBgBadge="bg-mandy-300" 
-                  colorTextBadge="text-mandy-800" 
-                  textBadge="Artigo" 
-                />
-                <ArticlePreview 
-                  banner="https://images.unsplash.com/photo-1587654780291-39c9404d746b?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  iconFill="" icon={Article} date="Há 15 dias" 
-                  link="posts/componentes-js-html" 
-                  title="Guia de Components - para quem tem pressa!" 
-                  description="Entendendo a base de Shadown DOM e componentização com JS em páginas HTML." 
-                  colorBgBadge="bg-mandy-300" 
-                  colorTextBadge="text-mandy-800" 
-                  textBadge="Artigo" 
-                />
-                <ArticlePreview 
-                  banner="https://images.unsplash.com/photo-1586717791821-3f44a563fa4c?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Ds"
-                  iconFill="fill-mandy-800" icon={World} date="Há 20 dias" 
-                  link="posts/guia-figma-devs"
-                  title="Guia de Figma para Devs" 
-                  description="Pra você que é dev e quer melhorar suas skills com figma e tirar aquele projetinho ou portfólio da cabeça sem precisar passar horas em um curso."
-                  colorBgBadge="bg-mandy-300"
-                  colorTextBadge="text-mandy-800"
-                  textBadge="Guia"
+              <ArticlePreview
+                banner="https://images.unsplash.com/photo-1648854006531-361649aa182c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                icon={Article} date="Há 1 mês"
+                link="posts/figma-fundamentos"
+                title="Figma para começar bem"
+                description="Esse post é para você que já se aventurou no figma mas não sabe aplicar as melhores práticas para otimizar sua vida ou de sua equipe."
+                colorBgBadge="bg-mandy-300"
+                colorTextBadge="text-mandy-800"
+                textBadge="Artigo"
+              />
+              <ArticlePreview
+                banner="https://images.unsplash.com/photo-1587654780291-39c9404d746b?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                iconFill="" icon={Article} date="Há 1 mês"
+                link="posts/componentes-js-html"
+                title="Guia de Components - para quem tem pressa!"
+                description="Entendendo a base de Shadown DOM e componentização com JS em páginas HTML."
+                colorBgBadge="bg-mandy-300"
+                colorTextBadge="text-mandy-800"
+                textBadge="Artigo"
+              />
+              <ArticlePreview
+                banner="https://images.unsplash.com/photo-1586717791821-3f44a563fa4c?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Ds"
+                iconFill="fill-mandy-800" icon={World} date="Em breve"
+                link="posts/guia-figma-devs"
+                title="Guia de Figma para Devs"
+                description="Pra você que é dev e quer melhorar suas skills com figma e tirar aquele projetinho ou portfólio da cabeça sem precisar passar horas em um curso."
+                colorBgBadge="bg-mandy-300"
+                colorTextBadge="text-mandy-800"
+                textBadge="Guia"
               />
             </div>
           </div>
--- src/components/Post/article.tsx
+++ src/components/Post/article.tsx
@@ -37,49 +37,49 @@ export default function ArticlePreview(props: any) {
       className="p-6 bg-white rounded-xl border border-white-200 shadow-md transition-all duration-300 hover:shadow-xl flex flex-col gap-4"
     >
       <div>
-        <div 
+        <div
           className="mb-4 relative"
         >
-          <Image src={props.banner} className="max-h-[224px] rounded-sm object-cover" width={672} height={224} alt="Banner"/>
+          <Image src={props.banner} className="max-h-[224px] rounded-sm object-cover" width={672} height={224} alt="Banner" />
 
           <div className="flex w-full justify-between items-center px-4 py-4 absolute top-0">
-            <span 
+            <span
               className={badgeStyles}
             >
-              <Image 
+              <Image
                 className="text-mandy-300"
                 width={16} height={16}
-                src={props.icon} alt="Icone de Câmera"/>
+                src={props.icon} alt="Icone de Câmera" />
               {props.textBadge}
             </span>
-            <span 
+            <span
               className="text-sm font-semibold py-1 px-2 bg-mandy-50 rounded bg-opacity-90 text-white-900">
               {props.date}
             </span>
           </div>
         </div>
         <div className="flex flex-col gap-2">
-          <h2 
-            className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          <h2
+            className="mb-2 text-xl md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
           >
-            <Link 
+            <Link
               href={props.link}
             >
               {props.title}
             </Link>
           </h2>
-          <p 
+          <p
             className="mb-5 font-light text-gray-500 dark:text-gray-400"
           >
             {props.description}
           </p>
         </div>
       </div>
-      <div 
+      <div
         className="flex justify-between items-center"
       >
-        <Link 
-          href={props.link} 
+        <Link
+          href={props.link}
           className="inline-flex items-center font-medium text-primary-600 gap-2 dark:text-primary-500 hover:underline"
         >
           Ler mais
