34fd9722fb0526b0bf0911229c9d717a59115ff1
AninhaPardini <anapardinisimoni@gmail.com>
2024-12-26 18:21:36 -0300

    fix: ajustado interação responsiva da navbar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import { Bars3Icon } from '@heroicons/react/16/solid';
-import { ArrowUpRightIcon } from '@heroicons/react/16/solid';
 
 import Logo from '@/assets/logo.svg';
 import Link from 'next/link';
@@ -11,13 +10,19 @@ import Image from 'next/image';
 const Links = [
   { href: '/', label: 'inicial', target: '_self' },
   { href: '/sobre', label: 'sobre', target: '_self' },
-  { href: 'https://www.figma.com/proto/12zPF3okZGsCJA2OlZbVev/Portif%C3%B3lio-V2?page-id=29%3A133&node-id=29-141&viewport=808%2C525%2C0.08&t=wmIZDeLrHwIKe12o-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=29%3A253', label: 'portfólio', target: '_blank'},
+  { href: 'https://www.figma.com/proto/12zPF3okZGsCJA2OlZbVev/Portif%C3%B3lio-V2?page-id=29%3A133&node-id=29-141&viewport=808%2C525%2C0.08&t=wmIZDeLrHwIKe12o-1&scaling=min-zoom&content-scaling=fixed&starting-point-node-id=29%3A253', label: 'portfólio', target: '_blank' },
 ];
 
 export default function Header() {
   const [scrollTop, setScrollTop] = useState(0);
   const [isScrollingDown, setIsScrollingDown] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+  //  const menu = document.getElementById('menu');
+  //  if (menu) {
+  //    isMenuOpen ? menu.className = 'hidden absolute top-[-20px]' : menu.className = 'flex absolute top-[20px]';
+  //  }
 
   useEffect(() => {
     let lastScrollTop = 0;
@@ -42,37 +47,37 @@ export default function Header() {
   }, []);
 
   return (
-    <nav 
-      className={`flex fixed max-w-[414px] md:max-w-[1536px] justify-between items-center w-full px-4 py-6 md:px-16 2xl:px-0 text-white-700 transition-all duration-500 ${isScrollingDown ? 'border-none -top-96' : 'bg-white-50 top-0 z-20 bg-opacity-95 '}`}
+    <nav
+      className={`w-[90vw] flex fixed max-w-[600px] md:max-w-[1536px] justify-between items-center md:w-full px-4 py-6 md:px-16 2xl:px-0 text-white-700 transition-all duration-500 ${isScrollingDown ? 'border-none -top-96' : 'bg-white-50 top-0 z-20 bg-opacity-95 '}`}
     >
-      
-      <Link 
+
+      <Link
         href='/'
       >
-        <Image 
+        <Image
           className='cursor-pointer w-16'
-          src={Logo} 
+          src={Logo}
           alt='Logo com as siglas AP'
         />
       </Link>
       <div
-          className={`duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${isMenuOpen ? 'top-[9%]' : 'top-[-100%]'} md:w-auto w-full flex items-center px-5`}>
-          <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
-              {Links.map(({ href, label, target }) => (
-                  <li key={`${href}${label}`}>
-                    <Link 
-                      href={href}
-                      target={target}
-                      className="text-white-800 hover:text-mandy-500 capitalize transition-all duration-300"
-                    >
-                      {label}
-                    </Link>
-                  </li>
-              ))}
-          </ul>
+        className={`md:flex transition-all duration-500 md:static top-24 md:top-0 absolute bg-white min-w-[400px] md:min-h-fit min-h-[60vh] left-0 md:w-auto w-full flex items-center px-5 ${isScrollingDown ? 'hidden' : ''} ${isMenuOpen ? 'flex shadow-lg bg-white-50 rounded-xl h-auto' : '-top-96 hidden'}`} id='menu'>
+        <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+          {Links.map(({ href, label, target }) => (
+            <li key={`${href}${label}`}>
+              <Link
+                href={href}
+                target={target}
+                className="text-white-800 hover:text-mandy-500 capitalize transition-all duration-300"
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="flex md:hidden items-center gap-6">
-        <button className='block md:hidden'>
+        <button className='z-10 block md:hidden' onClick={() => setIsMenuOpen(!isMenuOpen)}>
           <Bars3Icon className="w-12 h-12 cursor-pointer" />
         </button>
       </div>
